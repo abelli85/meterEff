3b0a970fe149a3418da76a72bcd962e1e4a712ed
abelli <abelli5@126.com>
2020-09-03 10:20:06 +0800

    client tests for data-service.

--- eff-api/src/test/kotlin/com/abel/bigwater/api/DataServiceTest.kt
+++ eff-api/src/test/kotlin/com/abel/bigwater/api/DataServiceTest.kt
@@ -8,6 +8,7 @@ import org.apache.http.entity.ContentType
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClients
 import org.jboss.resteasy.util.HttpResponseCodes
+import org.joda.time.LocalDate
 import org.joda.time.LocalDateTime
 import org.junit.Test
 import org.slf4j.LoggerFactory
@@ -35,14 +36,31 @@ class DataServiceTest {
         })
 
         try {
-            val url = TestHelper.URL_BASE + DataService.BASE_PATH + DataService.PATH_ADD_REALTIME_USER
-            val holder = BwHolder(TestHelper.buildLoginRequest(ul.single!!), list)
-            val post = HttpPost(url).apply {
-                entity = StringEntity(JSON.toJSONString(holder), ContentType.APPLICATION_JSON)
+            BwHolder(TestHelper.buildLoginRequest(ul.single!!), list).also { holder ->
+                val url = TestHelper.URL_BASE + DataService.BASE_PATH + DataService.PATH_ADD_REALTIME_USER
+
+                val post = HttpPost(url).apply {
+                    entity = StringEntity(JSON.toJSONString(holder), ContentType.APPLICATION_JSON)
+                }
+                val resp = HttpClients.createDefault().execute(post)
+                lgr.info("post realtime to {}: {} / {}", url, resp.statusLine, resp.entity.content.reader().readText())
+                assertEquals(HttpResponseCodes.SC_OK, resp.statusLine.statusCode)
             }
-            val resp = HttpClients.createDefault().execute(post)
-            lgr.info("post realtime to {}: {} / {}", url, resp.statusLine, resp.entity.content.reader().readText())
-            assertEquals(HttpResponseCodes.SC_OK, resp.statusLine.statusCode)
+
+            // list
+            BwHolder(TestHelper.buildLoginRequest(ul.single!!), DataParam(extId = "test-1").apply {
+                sampleTime1 = LocalDate(2020, 8, 1).toDate()
+                sampleTime2 = LocalDate(2020, 8, 2).toDate()
+            }).also { holder ->
+                val url = TestHelper.URL_BASE + DataService.BASE_PATH + DataService.PATH_LIST_REALTIME
+
+                val post = HttpPost(url).apply {
+                    entity = StringEntity(JSON.toJSONString(holder), ContentType.APPLICATION_JSON)
+                }
+                val resp = HttpClients.createDefault().execute(post)
+                lgr.info("list realtime to {}: {} / {}", url, resp.statusLine, resp.entity.content.reader().readText())
+                assertEquals(HttpResponseCodes.SC_OK, resp.statusLine.statusCode)
+            }
         } finally {
             // clear data
             val url = TestHelper.URL_BASE + DataService.BASE_PATH + DataService.PATH_DELETE_REALTIME
