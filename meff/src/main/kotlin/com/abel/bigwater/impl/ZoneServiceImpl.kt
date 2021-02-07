ae3639fa7d0f215a825bf79a1e24c05ff4621aab
abel <abelli5@qq.com>
2021-02-07 18:50:42 +0800

    working on zone-service.

--- meff/src/main/kotlin/com/abel/bigwater/impl/ZoneServiceImpl.kt
+++ meff/src/main/kotlin/com/abel/bigwater/impl/ZoneServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.abel.bigwater.impl
 
+import com.abel.bigwater.Helper
 import com.abel.bigwater.api.BwHolder
 import com.abel.bigwater.api.BwResult
 import com.abel.bigwater.api.MeterParam
@@ -35,7 +36,7 @@ class ZoneServiceImpl : ZoneService {
             return BwResult(2, ERR_PARAM)
         }
 
-        lgr.info("${holder.lr?.userId} try to list meter: ${JSON.toJSONString(holder.single)}")
+        lgr.info("${holder.lr?.userId} try to list zone: ${JSON.toJSONString(holder.single)}")
         val dp = holder.single!!
 
         val rightName = ZoneService.BASE_PATH + ZoneService.PATH_LIST_ZONE
@@ -54,37 +55,150 @@ class ZoneServiceImpl : ZoneService {
     }
 
     /**
-     * 获取一个分区的详情
+     * 创建分区, 包含其内部的总分表
+     */
+    override fun saveZone(holder: BwHolder<Zone>): BwResult<Zone> {
+        if (holder.lr?.sessionId.isNullOrBlank()) {
+            return BwResult(2, ERR_PARAM)
+        }
+
+        lgr.info("${holder.lr?.userId} try to save zone: ${JSON.toJSONString(holder.single)}")
+        val dp = holder.single!!
+
+        val rightName = ZoneService.BASE_PATH + ZoneService.PATH_SAVE_ZONE
+        try {
+            val login = loginManager!!.verifySession(holder.lr!!, rightName, rightName, JSON.toJSONString(holder.single));
+            if (login.code != 0) {
+                return BwResult(login.code, login.error!!)
+            }
+
+            val mlist = meterMapper!!.selectMeterZone(MeterParam().apply {
+                zoneId = dp.zoneId
+                meterIdList = dp.meterList?.map { it.meterId!! }
+                firmId = Helper.refineFirmId(dp.firmId, login.single!!.firmId!!)
+            })
+
+            dp.firmId = Helper.refineFirmId(dp.firmId, login.single!!.firmId!!, false)!!
+            zoneMapper!!.insertZone(dp)
+            zoneMapper!!.attachZoneMeter(dp)
+
+            return BwResult(dp)
+        } catch (ex: Exception) {
+            lgr.error(ex.message, ex);
+            return BwResult(1, "内部错误: ${ex.message}")
+        }
+    }
+
+    /**
+     * 获取一个分区的详情, 必填:
+     * @see MeterParam.zoneId
      */
     override fun fetchZone(holder: BwHolder<MeterParam>): BwResult<Zone> {
-        TODO("Not yet implemented")
+        if (holder.lr?.sessionId.isNullOrBlank()
+                || holder.single?.zoneId.isNullOrBlank()
+                || holder.single!!.zoneId.equals("%")) {
+            return BwResult(2, ERR_PARAM)
+        }
+
+        lgr.info("${holder.lr?.userId} try to list zone: ${JSON.toJSONString(holder.single)}")
+        val dp = holder.single!!
+
+        val rightName = ZoneService.BASE_PATH + ZoneService.PATH_LIST_ZONE
+        try {
+            val login = loginManager!!.verifySession(holder.lr!!, rightName, rightName, JSON.toJSONString(holder.single));
+            if (login.code != 0) {
+                return BwResult(login.code, login.error!!)
+            }
+
+            dp.refineFirmId(login.single!!)
+            val z = zoneMapper!!.listZone(dp).firstOrNull()?.also {
+                it.meterList = meterMapper!!.selectMeterZone(dp)
+                return BwResult(it)
+            }
+            return BwResult(0, "分区未找到: ${dp.zoneId}")
+        } catch (ex: Exception) {
+            lgr.error(ex.message, ex);
+            return BwResult(1, "内部错误: ${ex.message}")
+        }
     }
 
     /**
      * 更新一个分区, 包含其内部的总分表
      */
     override fun updateZone(holder: BwHolder<Zone>): BwResult<Zone> {
-        TODO("Not yet implemented")
+        if (holder.lr?.sessionId.isNullOrBlank()) {
+            return BwResult(2, ERR_PARAM)
+        }
+
+        lgr.info("${holder.lr?.userId} try to update zone: ${JSON.toJSONString(holder.single)}")
+        val dp = holder.single!!
+
+        val rightName = ZoneService.BASE_PATH + ZoneService.PATH_UPDATE_ZONE
+        try {
+            val login = loginManager!!.verifySession(holder.lr!!, rightName, rightName, JSON.toJSONString(holder.single));
+            if (login.code != 0) {
+                return BwResult(login.code, login.error!!)
+            }
+
+            dp.firmId = Helper.refineFirmId(dp.firmId, login.single!!.firmId!!, false)!!
+            val rcnt = zoneMapper!!.updateZone(dp)
+            return BwResult(dp).apply {
+                error = "修改分区: $rcnt"
+            }
+        } catch (ex: Exception) {
+            lgr.error(ex.message, ex);
+            return BwResult(1, "内部错误: ${ex.message}")
+        }
     }
 
     /**
      * 删除一个分区, 并解除分区跟水表的关联, 但不删除关联水表
      */
     override fun deleteZone(holder: BwHolder<Zone>): BwResult<Zone> {
-        TODO("Not yet implemented")
+        if (holder.lr?.sessionId.isNullOrBlank()) {
+            return BwResult(2, ERR_PARAM)
+        }
+
+        lgr.info("${holder.lr?.userId} try to delete zone: ${JSON.toJSONString(holder.single)}")
+        val dp = holder.single!!
+
+        val rightName = ZoneService.BASE_PATH + ZoneService.PATH_DELETE_ZONE
+        try {
+            val login = loginManager!!.verifySession(holder.lr!!, rightName, rightName, JSON.toJSONString(holder.single));
+            if (login.code != 0) {
+                return BwResult(login.code, login.error!!)
+            }
+
+            dp.firmId = Helper.refineFirmId(dp.firmId, login.single!!.firmId!!, false)!!
+            zoneMapper!!.detachZoneMeter(Zone().apply {
+                zoneId = dp.zoneId
+                firmId = dp.firmId
+            })
+
+            val rcnt = zoneMapper!!.deleteZone(MeterParam().apply {
+                zoneId = dp.zoneId
+                zoneName = dp.zoneName
+                firmId = dp.firmId
+            })
+
+            return BwResult(0, "删除分区: $rcnt")
+        } catch (ex: Exception) {
+            lgr.error(ex.message, ex);
+            return BwResult(1, "内部错误: ${ex.message}")
+        }
     }
 
     /**
      * 关联分区和水表
      */
-    override fun attachZoneMeter(holder: BwHolder<Zone>): BwResult<Zone> {
+    override fun saveZoneMeter(holder: BwHolder<Zone>): BwResult<Zone> {
         TODO("Not yet implemented")
     }
 
     /**
      * 解除关联分区和水表
      */
-    override fun detachZoneMeter(holder: BwHolder<Zone>): BwResult<Zone> {
+    override fun deleteZoneMeter(holder: BwHolder<Zone>): BwResult<Zone> {
         TODO("Not yet implemented")
     }
 
--- meff/src/test/kotlin/com/abel/bigwater/impl/ZoneServiceImplTest.kt
+++ meff/src/test/kotlin/com/abel/bigwater/impl/ZoneServiceImplTest.kt
@@ -8,6 +8,7 @@ import com.abel.bigwater.api.ZoneService
 import com.abel.bigwater.mapper.MapperTest
 import com.abel.bigwater.mapper.ZoneMapper
 import com.abel.bigwater.model.zone.Zone
+import com.abel.bigwater.model.zone.ZoneMeter
 import com.abel.bigwater.model.zone.ZoneType
 import com.alibaba.fastjson.JSON
 import org.apache.dubbo.remoting.http.servlet.ServletManager
@@ -38,13 +39,14 @@ class ZoneServiceImplTest {
     @Test
     fun listZone() {
         val z1 = Zone().apply {
-            firmId = "1"
             zoneId = "test"
             zoneName = "测试分区"
             zoneType = ZoneType.FAMILY.name
         }
 
         val login = TestHelper.login(us)
+        z1.firmId = login.single!!.firmId!!
+
         zs!!.listZone(BwHolder(TestHelper.buildLoginRequest(login.single!!),
                 MeterParam().apply {
                 })).also {
@@ -53,14 +55,14 @@ class ZoneServiceImplTest {
 
         try {
             zoneMapper!!.insertZone(z1)
-        } finally {
+
             zs!!.listZone(BwHolder(TestHelper.buildLoginRequest(login.single!!),
                     MeterParam().apply {
                     })).also {
                 lgr.info("list zone (1) : {}", JSON.toJSONString(it, true))
                 assertTrue(it.list!!.isNotEmpty())
             }
-
+        } finally {
             lgr.info("delete test zone: {}",
                     zoneMapper!!.deleteZone(MeterParam().apply {
                         zoneId = z1.zoneId
@@ -70,15 +72,144 @@ class ZoneServiceImplTest {
     }
 
     @Test
+    fun saveZone() {
+        val z1 = Zone().apply {
+            zoneId = "test"
+            zoneName = "测试分区"
+            zoneType = ZoneType.FAMILY.name
+            meterList = listOf(
+                    ZoneMeter().apply {
+                        meterId = "meter1"
+                        meterName = "水表1"
+                        meterCode = "meter-code-1"
+                    },
+                    ZoneMeter().apply {
+                        meterId = "meter2"
+                        meterName = "水表2"
+                        meterCode = "meter-code-2"
+                    }
+            )
+        }
+
+        val login = TestHelper.login(us)
+        z1.firmId = login.single!!.firmId!!
+
+        try {
+            zs!!.saveZone(BwHolder(TestHelper.buildLoginRequest(login.single!!), z1)).also {
+                lgr.info("save zone (1) : {}", JSON.toJSONString(it, true))
+                assertNotNull(it.single)
+                assertNull(it.list)
+            }
+        } finally {
+            lgr.info("delete test zone: {}/{}",
+                    zoneMapper!!.detachZoneMeter(Zone().apply {
+                        zoneId = z1.zoneId
+                    }),
+                    zoneMapper!!.deleteZone(MeterParam().apply {
+                        zoneId = z1.zoneId
+                        firmId = z1.firmId
+                    }))
+        }
+    }
+
+    @Test
     fun fetchZone() {
+        val z1 = Zone().apply {
+            zoneId = "test"
+            zoneName = "测试分区"
+            zoneType = ZoneType.FAMILY.name
+        }
+
+        val login = TestHelper.login(us)
+        z1.firmId = login.single!!.firmId!!
+
+        zs!!.fetchZone(BwHolder(TestHelper.buildLoginRequest(login.single!!),
+                MeterParam().apply {
+                    zoneId = "test"
+                })).also {
+            lgr.info("fetch zone (1) : {}", JSON.toJSONString(it, true))
+            assertNull(it.single)
+            assertEquals(0, it.list?.size ?: 0)
+        }
+
+        zoneMapper!!.insertZone(z1)
+        try {
+            zs!!.fetchZone(BwHolder(TestHelper.buildLoginRequest(login.single!!),
+                    MeterParam().apply {
+                        zoneId = "test"
+                    })).also {
+                lgr.info("fetch zone (1) : {}", JSON.toJSONString(it, true))
+                assertNotNull(it.single)
+                assertNull(it.list)
+            }
+        } finally {
+            lgr.info("delete test zone: {}",
+                    zoneMapper!!.deleteZone(MeterParam().apply {
+                        zoneId = z1.zoneId
+                        firmId = z1.firmId
+                    }))
+        }
     }
 
     @Test
     fun updateZone() {
+        val z1 = Zone().apply {
+            zoneId = "test"
+            zoneName = "测试分区"
+            zoneType = ZoneType.FAMILY.name
+        }
+
+        val login = TestHelper.login(us)
+        z1.firmId = login.single!!.firmId!!
+
+        zoneMapper!!.insertZone(z1)
+        try {
+            zs!!.updateZone(BwHolder(TestHelper.buildLoginRequest(login.single!!), z1.apply {
+                zoneName = "test zone 2"
+            })).also {
+                lgr.info("update test zone: {}", JSON.toJSONString(it, true))
+            }
+
+            zoneMapper!!.listZone(MeterParam(zoneId = z1.zoneId).apply {
+                firmId = login.single!!.firmId
+            }).first().also {
+                assertEquals(z1.zoneId, it.zoneId)
+                assertEquals(z1.zoneName, it.zoneName)
+            }
+        } finally {
+            lgr.info("delete test zone: {}",
+                    zoneMapper!!.deleteZone(MeterParam().apply {
+                        zoneId = z1.zoneId
+                        firmId = z1.firmId
+                    }))
+        }
     }
 
     @Test
     fun deleteZone() {
+        val z1 = Zone().apply {
+            zoneId = "test"
+            zoneName = "测试分区"
+            zoneType = ZoneType.FAMILY.name
+        }
+
+        val login = TestHelper.login(us)
+        z1.firmId = login.single!!.firmId!!
+
+        zoneMapper!!.insertZone(z1)
+        try {
+            zs!!.deleteZone(BwHolder(TestHelper.buildLoginRequest(login.single!!), z1.apply {
+            })).also {
+                lgr.info("delete test zone: {}", JSON.toJSONString(it, true))
+                assertEquals(0, it.code)
+            }
+        } finally {
+            lgr.info("delete test zone: {}",
+                    zoneMapper!!.deleteZone(MeterParam().apply {
+                        zoneId = z1.zoneId
+                        firmId = z1.firmId
+                    }))
+        }
     }
 
     @Test
